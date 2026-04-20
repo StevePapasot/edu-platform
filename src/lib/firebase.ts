1f9bf346b1e9925316f5355ab85c8b26baf0612f
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2026-04-20 17:25:52 +0300

    Updated firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
@@ -13,7 +13,8 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
