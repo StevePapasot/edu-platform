f0dc91fab4b25be9b2a1369e325aa6e494ceb351
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2026-04-24 10:12:53 +0300

    Updated page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import {
   ShieldAlert, ChevronRight, BookOpen, GraduationCap, UserCog,
   Loader2, Gift, MonitorPlay, Award, Palette, ShieldCheck, X,
   Trophy, CheckCircle2, XCircle, RotateCcw, ChevronLeft, Compass
-} from 'lucide-react';
+} from 'lucide-react';a
 
 import { auth, db } from '@/src/lib/firebase';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
